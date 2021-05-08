473c05848ceede4ecb266cf9b9981d4cd29b1b1e
Pål Sollie <sollie@sparkz.no>
2021-05-08 20:17:02 +0200

    initial

---
+++ main.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+puppeteer.launch().then(async browser => {
+
+  const page = await browser.newPage();
+  let url = 'https://www.naaf.no/pollenvarsel/';
+  await page.goto(url, {'waitUntil': 'networkidle0', 'timeout': 0}).catch(e => {
+    console.log(e.toString());
+  });
+
+  var pollenNames = await page.evaluate(() => pollenNames);
+  var area = await page.evaluate(() => areas);
+
+  var today = await page.evaluate(() => document.querySelector('#todaysdate').innerText);
+  var tomorrow = await page.evaluate(() => document.querySelector('#tomorrowsdate').innerText);
+
+  await browser.close();
+
+  var data = {areas: area, names: pollenNames, dates: {today: today.slice(-10), tomorrow: tomorrow.slice(-10)}};
+
+  console.log(JSON.stringify(data));
+
+});
